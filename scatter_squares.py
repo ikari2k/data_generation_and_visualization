7f69f1ad0ac24c446d88b143191b67fb729b11fb
ikari2k <pluta.m@gmail.com>
2024-03-04 10:51:04 +0100

    scatter function in use

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
 plt.style.use("ggplot")
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=100)
 
 # Define chart title and axes labels
 ax.set_title("Squares", fontsize=24)
@@ -14,4 +14,5 @@ ax.set_ylabel("Values squares", fontsize=14)
 
 # Define labels size
 ax.tick_params(labelsize=14)
+
 plt.show()
