efbc622cef96b44162d486f75c8362dd8cc59f57
ikari2k <pluta.m@gmail.com>
2024-03-04 10:54:21 +0100

    generating plot values

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use("ggplot")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Define chart title and axes labels
 ax.set_title("Squares", fontsize=24)
@@ -14,5 +14,8 @@ ax.set_ylabel("Values squares", fontsize=14)
 
 # Define labels size
 ax.tick_params(labelsize=14)
+
+# Define range for axes
+ax.axis([0, 1100, 0, 1_100_000])
 
 plt.show()
