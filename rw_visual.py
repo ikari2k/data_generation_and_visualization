a05057900a58fd1515ed88ce56eb2c73fc791cdc
ikari2k <pluta.m@gmail.com>
2024-03-04 11:28:01 +0100

    keep creating plots

--- rw_visual.py
+++ rw_visual.py
@@ -2,16 +2,23 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Prepare data
-rw = RandomWalk()
-rw.fill_walk()
+# Prepare new random walk until program is active
+while True:
 
-coordinates = tuple(zip(rw.x_values, rw.y_values))
-print(coordinates)
+    # Prepare data
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Display data points
-plt.style.use("classic")
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect("equal")  # setting equal aspect ratio for each axis
-plt.show()
+    coordinates = tuple(zip(rw.x_values, rw.y_values))
+    print(coordinates)
+
+    # Display data points
+    plt.style.use("classic")
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect("equal")  # setting equal aspect ratio for each axis
+    plt.show()
+
+    keep_running = input("Create new random walk plot? (y/n): ")
+    if keep_running == "n":
+        break
