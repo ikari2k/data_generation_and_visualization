6903eb56fb8f7944abb5711fc65a4b9dafffb701
ikari2k <pluta.m@gmail.com>
2024-03-05 16:44:37 +0100

    construct geo plot with earthquakes

--- eq_explore_dataset.py
+++ eq_explore_dataset.py
@@ -1,11 +1,22 @@
 from pathlib import Path
 import json
+import plotly.express as px
 
 path = Path("mapping_datasets/eq_data_1_day_m1.geojson")
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
-# Dump data in more readable format
-path = Path("mapping_datasets/readable_eq_data.geojson")
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+all_eq_dicts = all_eq_data["features"]
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict["properties"]["mag"]
+    lon = eq_dict["geometry"]["coordinates"][0]
+    lat = eq_dict["geometry"]["coordinates"][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+title = "Earthquakes on earth"
+fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig.show()
