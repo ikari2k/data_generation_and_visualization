d38bc563b84801823c9f576cf007825eb5d603aa
ikari2k <pluta.m@gmail.com>
2024-03-06 14:33:22 +0100

    analyzing response from GH API

--- python_repos.py
+++ python_repos.py
@@ -10,6 +10,15 @@ print(f"Status code: {r.status_code}")
 
 # Convert response to dictionary
 response_dict = r.json()
+print(f'Total number of repos: {response_dict["total_count"]}')
+print(f'Complete result: {not response_dict["incomplete_results"]}')
 
 # Process the results
-print(response_dict.keys())
+repo_dicts = response_dict["items"]
+print(f"Number of returned repos: {len(repo_dicts)}")
+
+# Analyze first repo
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
